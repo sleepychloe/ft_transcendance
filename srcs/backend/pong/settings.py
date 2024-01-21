f9d147f09261039f0f1e1aeb9869ab72d97d4f24
YugeonCHUN <PfClaKr@github.com>
2024-01-21 19:03:33 +0100

    [FEATURE] add postgresql and connected with django

--- srcs/backend/pong/settings.py
+++ srcs/backend/pong/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'pong.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+db_user=os.environ.get('DB_USER')
+db_password=os.environ.get('DB_PASSWORD')
+db_host=os.environ.get('DB_HOST')
+db_port=os.environ.get('DB_PORT')
+postgres_db=os.environ.get('POSTGRES_DB')
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': postgres_db,
+		'USER': db_user,
+		'PASSWORD': db_password,
+		'HOST': db_host,
+		'PORT': db_port,
     }
 }
 
@@ -123,3 +133,36 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,      #디폴트 : True, 장고의 디폴트 로그 설정을 대체. / False : 장고의 디폴트 로그 설정의 전부 또는 일부를 다시 정의
+#     'formatters': {                        # message 출력 포맷 형식
+#         'verbose': {
+#             'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+#             'datefmt' : "%d/%b/%Y %H:%M:%S"
+#         },
+#         'simple': {
+#             'format': '%(levelname)s %(message)s'
+#         },
+#     },
+#     'handlers': {
+#         'logstash': {
+#             'level': 'DEBUG',
+#             'class': 'logging.FileHandler',
+#             'filename': 'file_name.log',        # message가 저장될 파일명(파일명 변경 가능)
+#             'formatter': 'verbose'
+#         },
+#     },
+#     'loggers': {
+#         'django': {
+#             'handlers':['file'],        # 'file' : handler의 이름
+#             'propagate': True,         
+#             'level':'DEBUG',            # DEBUG 및 그 이상의 메시지를 file 핸들러에게 보내줍니다.
+#         },
+#         'app_name': {                   # Project에서 생성한 app의 이름
+#             'handlers': ['file'],          # 다른 app을 생성 후 해당 app에서도
+#             'level': 'DEBUG',          # 사용하고자 할 경우 해당 app 이름으로
+#         },                                      # 좌측 코드를 추가 작성해서 사용
+#     }
+# }​
