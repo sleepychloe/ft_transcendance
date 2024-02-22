6bce3cd9d793301e0c7fba93f94fe0a120907ff0
YugeonCHUN <PfClaKr@github.com>
2024-02-22 18:44:26 +0100

    [FIX] MultiGameConsumer strcuture done, database_sync_to_async decorator add

--- srcs/backend/game/consumers.py
+++ srcs/backend/game/consumers.py
@@ -1,7 +1,8 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.http import JsonResponse
 from .models import MultiRoomInfo
-from asgiref.sync import async_to_sync
+from asgiref.sync import sync_to_async
+from channels.db import database_sync_to_async
 import random
 import http.cookies
 import json
@@ -10,26 +11,31 @@ import time
 
 class MultiGameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        await self.accept()
         request_header = self.scope['headers']
         cookie_header = next((header for header in request_header if header[0] == b'cookie'), None)
         if cookie_header:
             cookies = cookie_header[1].decode('utf-8')
-        client_id = cookies.lstrip("client_id=")
-        await self.accept()
+        self.client_id = cookies.lstrip("client_id=")
+        if self.client_id == cookies:
+            self.send(text_data=json.dumps({'Error': 'Can not found your id!'}))
+            self.close()
         game_id = self.scope["url_route"]["kwargs"]["game_id"]
-        await self.send(text_data=json.dumps({'client_id': client_id, 'game_id': game_id}))
         self.game_id = game_id
-        self.initialize_game(game_id)
+        try :
+            self.game_data = sync_to_async(MultiRoomInfo.objects.get(RoomName=game_id))
+        except MultiRoomInfo.DoesNotExist:
+            await self.send(text_data=json.dumps({'Error': 'Wrong Url ! (can not match game id)'}))
+            self.close()  
+        self.game_start = False
+        self.initialize_game(self)
         self.channel_layer.group_add(game_id, self.channel_name)
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.game_id, self.channel_name)
 
-    async def init_game_value(self, game_id):
-        try :
-            game_data = MultiRoomInfo.objects.get(RoomName=game_id)
-        except MultiRoomInfo.DoesNotExist:
-             self.send(text_data=json.dumps({'Error': 'Game id URL is not exists in init_game_value'}))
+    @database_sync_to_async
+    def init_game_value(self):
         self.game_state = {
             'ball': {
                 'x': 400,
@@ -37,130 +43,138 @@ class MultiGameConsumer(AsyncWebsocketConsumer):
                 'vx': 2 + 1.5 * random.random(),
                 'vy': 2 + 1.5 * random.random(),
             },
-            game_data.paddle1: {
+            self.game_data.paddle1: {
                 'x': 0,
                 'y': 150,
             },
-            game_data.paddle2: {
+            self.game_data.paddle2: {
                 'x': 20,
                 'y': 150,
             },
-            game_data.paddle3: {
-                'x': 800,
+            self.game_data.paddle3: {
+                'x': 790,
                 'y': 150,
             },
-            game_data.paddle4: {
-                'x': 780,
+            self.game_data.paddle4: {
+                'x': 770,
                 'y': 150,
             },
         }
+        if self.game_start == False:
+            self.game_score = {
+                'left': 0,
+                'right': 0,
+            },
 
-
-    async def init_game_paddle(self, game_id):
-        try :
-            game_data = MultiRoomInfo.objects.get(RoomName=game_id)
-        except MultiRoomInfo.DoesNotExist:
-            await self.send(text_data=json.dumps({'Error': 'Game id URL is not exists in init_game_paddle'}))
+    @database_sync_to_async
+    def init_game_paddle(self):
         if self.client_id == None:
              return self.send(json.dumps({'Error': 'Client id is not exists'}))
-        if game_data.paddle1 == None:
-             game_data.paddle1 = self.client_id
-             game_data.save()
-        elif game_data.paddle2 == None:
-             game_data.paddle2 = self.client_id
-             game_data.save()
-        elif game_data.paddle3 == None:
-             game_data.paddle3 = self.client_id
-             game_data.save()
-        elif game_data.paddle4 == None:
-             game_data.paddle4 = self.client_id
-             game_data.save()
+        if self.game_data.paddle1 == None:
+             self.game_data.paddle1 = self.client_id
+             self.game_data.save()
+        elif self.game_data.paddle2 == None:
+             self.game_data.paddle2 = self.client_id
+             self.game_data.save()
+        elif self.game_data.paddle3 == None:
+             self.game_data.paddle3 = self.client_id
+             self.game_data.save()
+        elif self.game_data.paddle4 == None:
+             self.game_data.paddle4 = self.client_id
+             self.game_data.save()
 
-    async def initialize_game(self, game_id):
-        self.init_game_paddle(self, game_id)
-        self.init_game_value(self, game_id)
-        self.send_game_state(self, game_id)
+    async def initialize_game(self):
+        await self.init_game_paddle(self)
+        await self.init_game_value(self)
+        await self.init_game_state(self)
 
     
     async def init_thread_ball(self):
         self.thread = threading.Thread(target=ball_move_thread, args=[self])
         self.thread.setDaemon(True)
 
-    async def send_game_state(self, game_id):
-        try :
-            game_data = MultiRoomInfo.objects.get(RoomName=game_id)
-        except MultiRoomInfo.DoesNotExist:
-             self.send(text_data=json.dumps({'Error': 'Game id URL is not exists in init_game_paddle'}))
-        if game_data.paddle1 and game_data.paddle2 and game_data.paddle3 and game_data.paddle4:
+    async def init_game_state(self):
+        if self.game_data.paddle1 and self.game_data.paddle2 and self.game_data.paddle3 and self.game_data.paddle4:
             self.init_thread_ball()
-            await self.send(text_data=json.dumps({'type': 'game_state', 'data': self.game_state}))
+            await self.send(text_data=json.dumps({'type': 'game_state', 'data': self.game_state + self.game_score}))
 
-    # async def update_game_state(self):
-    #     # Update the ball's position based on its velocity
-    #     self.game_state['ball']['x'] += self.game_state['ball']['vx']
-    #     self.game_state['ball']['y'] += self.game_state['ball']['vy']
-
-    #     # Boundary collision detection for an 800x600 canvas
-    #     if self.game_state['ball']['x'] <= 0 or self.game_state['ball']['x'] >= 800:
-    #         self.game_state['ball']['vx'] = -self.game_state['ball']['vx']
-    #     if self.game_state['ball']['y'] <= 0 or self.game_state['ball']['y'] >= 400:
-    #         self.game_state['ball']['vy'] = -self.game_state['ball']['vy']
-
-    async def change_user_status(self, n_client, game_data):
+    @database_sync_to_async
+    def change_user_status(self, n_client):
         if n_client == 'client1':
-            game_data.client1['ready_status'] = 1
+            self.game_data.client1['ready_status'] = 1
         elif n_client == 'client2':
-            game_data.client2['ready_status'] = 1
+            self.game_data.client2['ready_status'] = 1
         elif n_client == 'client3':
-            game_data.client3['ready_status'] = 1
+            self.game_data.client3['ready_status'] = 1
         elif n_client == 'client4':
-            game_data.client4['ready_status'] = 1
-        await game_data.save()
+            self.game_data.client4['ready_status'] = 1
+        self.game_data.save()
 
-
-    async def check_user_all_ready(self, game_data):
-        if game_data.client1['ready_status'] == 1 and game_data.client2['ready_status'] == 1 and game_data.client3['ready_status'] == 1 and game_data.client4['ready_status'] == 1:
-            await self.send(text_data=json.dumps({'game_start': 'ok'}))
+    @database_sync_to_async
+    def check_user_all_ready(self):
+        if self.game_data.client1['ready_status'] == 1 and self.game_data.client2['ready_status'] == 1 and self.game_data.client3['ready_status'] == 1 and self.game_data.client4['ready_status'] == 1:
+            self.send(text_data=json.dumps({'game_start': 'ok'}))
+            time.sleep(3)
             self.thread.start()
+            self.game_start = True
 
-    async def receive(self, game_id, text_data=None, bytes_data=None):
-        try :
-            game_data = MultiRoomInfo.objects.get(RoomName=game_id)
-        except MultiRoomInfo.DoesNotExist:
-             await self.send(text_data=json.dumps({'Error': 'Game id URL is not exists in init_game_paddle'}))
+    async def send_game_state(self):
+        self.channel_layer.group_send(self.game_state)
+
+    async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         action = text_data_json.get('action')
         status = text_data_json.get('user_status')
-        await self.check_user_all_ready(self, game_data)
 
         if status == 'user_status':
             n_client = text_data_json.get('n_client')
-            self.change_user_status(self, n_client, game_data)
-        if action == 'move_paddle':
+            await self.change_user_status(self, n_client)
+            await self.check_user_all_ready(self)
+        elif action == 'move_paddle' and self.game_start == True:
                 direction = text_data_json.get('direction')
-                self.move_paddle(direction)
-                self.send_game_state()
-        # elif action == 'update':
-        #         self.update_game_state()
-        #         self.send_game_state()
+                await self.move_paddle(direction)
+                self.send_game_state(self)
         else:
-                print("Received unknown action:", action)
+            self.send(text_data=json.dumps({'Error': 'There is nothings to do!'}))
 
-    async def move_paddle(self, direction, client_id):
+    async def move_paddle(self, direction):
         if direction == 'down':
-            if self.game_state['paddle']['y'] < 300:
-                self.game_state['paddle']['y'] += 10
+            if self.game_state[self.client_id]['y'] < 300:
+                self.game_state[self.client_id]['y'] += 15
         elif direction == 'up':
-            if self.game_state['paddle']['y'] > 0:
-                self.game_state['paddle']['y'] -= 10
+            if self.game_state[self.client_id]['y'] > 0:
+                self.game_state[self.client_id]['y'] -= 15
+        else:
+            self.send(text_data=json.dumps({'Error': 'Can not understand paddle dircection !'}))
 
 def ball_move_thread(self):
     while True:
         time.sleep(0.05)
+        paddle_ball_collision(self)
         self.game_state['ball']['x'] += self.game_state['ball']['vx']
         self.game_state['ball']['y'] += self.game_state['ball']['vy']
         if self.game_state['ball']['x'] <= 0 or self.game_state['ball']['x'] >= 800:
-            self.game_state['ball']['vx'] = -self.game_state['ball']['vx']
+            if self.game_state['ball']['x'] <= 0:
+                self.game_score['right'] += 1
+            else:
+                self.game_score['left'] += 1
+            self.init_game_value(self)
         if self.game_state['ball']['y'] <= 0 or self.game_state['ball']['y'] >= 400:
            self.game_state['ball']['vy'] = -self.game_state['ball']['vy']
         self.channel_layer.group_send(self.game_state)
+
+def paddle_ball_collision(self):
+    left_paddle1 = self.game_data.paddle1
+    left_paddle2 = self.game_data.paddle2
+    right_paddle3 = self.game_data.paddle3
+    right_paddle4 = self.game_data.paddle4
+    ball_x = self.game_state['ball']['x']
+    ball_y = self.game_state['ball']['y']
+    paddle_width = 10
+    paddle_height = 100
+    canvas_width = 800
+    if ((ball_x < paddle_width and ball_y > left_paddle1['y'] and ball_y < left_paddle1['y'] + paddle_height) or 
+        (ball_x + 20 < paddle_width and ball_y > left_paddle2['y'] and ball_y < left_paddle2['y'] + paddle_height) or
+        (ball_x > canvas_width - paddle_width and ball_y > right_paddle3['y'] and ball_y < right_paddle3['y'] + paddle_height) or
+        (ball_x - 20 > canvas_width - paddle_width and ball_y > right_paddle4['y'] and ball_y < right_paddle4['y'] + paddle_height)) :
+        self.game_state['ball']['vx'] = -self.game_state['ball']['vx']
